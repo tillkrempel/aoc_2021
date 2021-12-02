3c0afc82d12ca3caee03227d91bbb7f1e0c235cd
Till Krempel <till.krempel@cologne-intelligence.de>
2021-12-02 20:41:48 +0100

    Day 01 part 2 finished

--- src/Day01.kt
+++ src/Day01.kt
@@ -2,7 +2,7 @@ fun main() {
     fun part1(input: List<String>): Int {
         var first: Int? = null
         var raises = 0
-        input.forEach { line->
+        input.forEach { line ->
             (Integer.parseInt(line)).let { number ->
                 first?.let {
                     if (number > it) raises++
@@ -14,12 +14,23 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        // Compare Sliding window of width 3
+        var last: Int? = null
+        var raises = 0
+        input.windowed(3, 1, false) { window ->
+            val actual = window.sumOf { Integer.parseInt(it) }
+            last?.let {
+                if (actual > it) raises++
+            }
+            last = actual
+        }
+        return raises
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
     check(part1(testInput) == 7)
+    check(part2(testInput) == 5)
 
     val input = readInput("Day01")
     println(part1(input))
