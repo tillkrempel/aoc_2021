b455e76eb34341322f5ab3cb6019ea8acca1d7ae
Till Krempel <till.krempel@cologne-intelligence.de>
2021-12-02 20:33:04 +0100

    Day 01 finished

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,6 +1,16 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var first: Int? = null
+        var raises = 0
+        input.forEach { line->
+            (Integer.parseInt(line)).let { number ->
+                first?.let {
+                    if (number > it) raises++
+                }
+                first = number
+            }
+        }
+        return raises
     }
 
     fun part2(input: List<String>): Int {
@@ -9,7 +19,7 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 7)
 
     val input = readInput("Day01")
     println(part1(input))
